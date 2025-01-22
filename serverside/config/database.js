bc2649ebb5ebd71315375b82bcec27b803a1d31a
MaximkaProMax <Zholdashev.Maks@yandex.ru>
2025-01-22 20:20:55 +0300

    Hot Fix 3

--- serverside/config/database.js
+++ serverside/config/database.js
@@ -2,8 +2,8 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('photoproject', 'Max', 'Max', {
-  host: '127.0.0.1',
-  dialect: 'postgres',
+    host: '127.0.0.1',
+    dialect: 'postgres',
 });
 
 module.exports = sequelize;
--- serverside/routes/reviewRoutes.js
+++ serverside/routes/reviewRoutes.js
@@ -1,7 +1,9 @@
+// routes/reviewRoutes.js
 const express = require('express');
-const Review = require('../models/Review');
+const Review = require('../models/Reviews');  // Путь должен указывать на 'Reviews'
 const router = express.Router();
 
+// Маршруты для отзывов
 router.get('/reviews', async (req, res) => {
   try {
     const reviews = await Review.findAll();
