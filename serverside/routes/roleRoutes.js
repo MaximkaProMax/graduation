fd77c5b5b3d3e1f1100987e9864b4848a34e0956
MaximkaProMax <Zholdashev.Maks@yandex.ru>
2025-01-31 15:29:57 +0300

    Connecting User Groups

--- serverside/routes/roleRoutes.js
+++ serverside/routes/roleRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../config/database'); // Импортируем настройку подключения к базе данных
+const sequelize = require('../config/database'); // Импортируем настройку подключения к базе данных
 
 // Маршрут для получения всех ролей
 router.get('/', async (req, res) => {
   try {
     console.log('Запрос к базе данных перед выполнением: SELECT * FROM "Roles"');
-    const roles = await pool.query('SELECT * FROM "Roles"');
-    console.log('Полный ответ от базы данных:', roles); // Отладочный вывод всей структуры ответа
-    console.log('Роли, возвращенные с сервера:', roles.rows); // Отладочный вывод конкретных данных
-    if (roles.rows && roles.rows.length > 0) {
-      res.json(roles.rows);
+    const [roles, metadata] = await sequelize.query('SELECT * FROM "Roles"');
+    console.log('Роли, возвращенные с сервера:', roles); // Отладочный вывод конкретных данных
+
+    if (roles && roles.length > 0) {
+      res.json(roles);
     } else {
       console.log('Нет ролей в базе данных');
-      res.status(204).json({ error: 'Нет данных' });
+      res.status(204).send();
     }
   } catch (err) {
     console.error('Ошибка при получении данных о ролях:', err.message);
