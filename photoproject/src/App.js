1030a5a9675b6eb7b69c0e7400c05ed8c5b6981a
MaximkaProMax <Zholdashev.Maks@yandex.ru>
2025-02-01 18:33:50 +0300

    Fixing the footer

--- photoproject/src/App.js
+++ photoproject/src/App.js
@@ -8,24 +8,24 @@ import Home from './components/Home';
 import Login from './components/Login'; // Импортируем компонент Login
 import Registration from './components/Registration'; // Импортируем компонент Registration
 import Footer from './components/Footer'; // Импортируем компонент Footer
-import PrintingLayFlat from './components/PrintingLayFlat'; // Импортируйте компонент PrintingLayFlat
+import PrintingLayFlat from './components/PrintingLayFlat'; // Импортируем компонент PrintingLayFlat
 import PrintingFlexBind from './components/PrintingFlexBind';
-import Calendar from './components/Calendar'; // Импортируйте компонент Calendar
+import Calendar from './components/Calendar'; // Импортируем компонент Calendar
 import Cart from './components/Cart'; // Импорт компонента Cart
 import Payment from './components/Payment'; // Импорт компонента Payment
 import Reviews from './components/Reviews'; // Импорт компонента Reviews
 import Admin from './components/Admin'; // Импорт компонента Admin
 import EditUserGroups from './components/EditUserGroups'; // Импорт компонента EditUserGroups
-import EditDatabase from './components/EditDatabase'
+import EditDatabase from './components/EditDatabase';
 import EditUsers from './components/EditUsers';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="wrapper">
         <Header />
-        <main>
+        <main className="content">
           <Routes>
             <Route path="/photostudios" element={<Photostudios />} />
             <Route path="/printing" element={<Printing />} />
@@ -45,7 +45,7 @@ function App() {
             <Route path="/admin/edit-database" element={<EditDatabase />} /> {/* Редактирование БД */}
           </Routes>
         </main>
-        <Footer /> {/* Располагаем футер здесь */}
+        <Footer />
       </div>
     </Router>
   );
