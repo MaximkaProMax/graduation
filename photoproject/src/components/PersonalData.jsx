b188e357e5728c70b8b758ee66e9d84cc1f36942
MaximkaProMax <Zholdashev.Maks@yandex.ru>
2025-05-04 23:30:17 +0300

    Update PersonalData.jsx

--- photoproject/src/components/PersonalData.jsx
+++ photoproject/src/components/PersonalData.jsx
@@ -9,16 +9,12 @@ const PersonalData = () => {
     navigate('/manager/edit-personal-data'); // Переход на страницу редактирования личных данных
   };
 
-  const handleViewRequests = () => {
-    navigate('/manager/requests'); // Переход на страницу заявок
-  };
-
   return (
     <div className="personal-data-container">
       <h2>Управление личными данными</h2>
       <div className="buttons-container">
         <button className="personal-data-button" onClick={handleEditPersonalData}>Редактирование личных данных</button>
-        <button className="personal-data-button" onClick={handleViewRequests}>Просмотр заявок</button>
+        {/* Кнопка "Просмотр заявок" удалена */}
       </div>
     </div>
   );
