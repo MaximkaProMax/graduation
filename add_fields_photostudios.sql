b6feb0eeed4a8271b173fd0ccc532f0e4ae959c5
MaximkaProMax <Zholdashev.Maks@yandex.ru>
2025-05-08 12:18:39 +0300

    Sql query deleted

--- add_fields_photostudios.sql
+++
@@ -1,6 +0,0 @@
--- Добавить поля contact_information, description, booking в таблицу photostudios
-
-ALTER TABLE photostudios
-ADD COLUMN contact_information VARCHAR(255),
-ADD COLUMN description TEXT,
-ADD COLUMN booking BOOLEAN;
